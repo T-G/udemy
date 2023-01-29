4fcedc33457fd5ea6450566dc68f62beed5c2865
Theo <pronoygomes@yahoo.com>
2023-01-29 20:01:13 +0000

    added template parts, separated header, main and footer block in index.html

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@
 
 // Includes
 include(get_theme_file_path( '/includes/frontend/enqueue.php' ));
-
+include(get_theme_file_path( '/includes/frontend/head.php' ));
 
 
 // Hooks
 add_action('wp_enqueue_scripts', 'u_enqueue');
+add_action("wp_head", 'u_head', 5);
